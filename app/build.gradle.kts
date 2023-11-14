11f403c06ef679e18071a8cf31f533e6b1b952be
thadeus <thadeusnyariki94@gmail.com>
2023-11-14 23:20:44 +0300

    still issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,10 @@ android {
 
 dependencies {
 
+    //noinspection GradleCompatible
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core:1.9.0")
+    implementation ("androidx.fragment:fragment:1.3.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
@@ -43,6 +46,6 @@ dependencies {
      * Added extra dependencies step: 1
      * */
 //    implementation("com.google.android.material:material:1.4.0-rc01")
-//    implementation("com.github.bumptech.glide:glide:4.8.0")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
 //    implementation("com.google.code.gson:gson:2.8.7")
 }
--- app/src/main/java/com/example/foodapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/foodapp/Activity/MainActivity.java
@@ -2,19 +2,45 @@ package com.example.foodapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 
+import com.example.foodapp.Activity.Adaptor.CategoryAdaptor;
+import com.example.foodapp.Activity.Domain.CategoryDomain;
 import com.example.foodapp.R;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView.Adapter adapter;
+    private RecyclerView recyclerViewCategoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerViewCategory();
+    }
+
+    private void recyclerViewCategory() {
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
+        recyclerViewCategoryList=findViewById(R.id.recyclerView);
+        recyclerViewCategoryList.setLayoutManager(linearLayoutManager);
+
+        ArrayList<CategoryDomain> category=new ArrayList<>();
+        category.add(new CategoryDomain("Pizza", "cat_1"));
+        category.add(new CategoryDomain("Burger", "cat_2"));
+        category.add(new CategoryDomain("Hot dog", "cat_3"));
+        category.add(new CategoryDomain("Drink", "cat_4"));
+        category.add(new CategoryDomain("Doughnut", "cat_5"));
+
+        adapter = new CategoryAdaptor(category);
+        recyclerViewCategoryList.setAdapter(adapter);
     }
 
     public static class IntroActivity extends AppCompatActivity {
